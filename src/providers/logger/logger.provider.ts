6353bb0d07cae862987632384aa27a0f2db61af5
Anderson <anderson.filho@catenait.com.br>
2023-08-10 02:28:48 -0300

    feat: add logger obfuscator

--- src/providers/logger/logger.provider.ts
+++ src/providers/logger/logger.provider.ts
@@ -1,22 +1,32 @@
 import { Logger as WinstonLogger } from 'winston';
 import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { LoggerProviderInterface } from './logger.provider.interface';
+import { obfuscatorObject } from '@src/utils/obfuscator-object';
 
 @Injectable()
-export class LoggerProvider implements LoggerService {
+export class LoggerProvider implements LoggerService, LoggerProviderInterface {
+  typeAcceptToFormats: string[];
   constructor(
     @Inject(WINSTON_MODULE_NEST_PROVIDER) private loggerWinston: WinstonLogger,
-  ) {}
+  ) {
+    this.typeAcceptToFormats = ['number', 'string'];
+  }
+
   error(message: any, ...optionalParams: any[]) {
+    obfuscatorObject(optionalParams);
     this.loggerWinston.error(message, optionalParams);
   }
   warn(message: any, ...optionalParams: any[]) {
+    obfuscatorObject(optionalParams);
     this.loggerWinston.warn(message, optionalParams);
   }
   log(message: any, ...optionalParams: any[]) {
+    obfuscatorObject(optionalParams);
     this.loggerWinston.log(message, optionalParams);
   }
   info(message: any, ...optionalParams: any[]) {
+    obfuscatorObject(optionalParams);
     this.loggerWinston.log(message, optionalParams);
   }
 }
